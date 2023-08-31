36db47adc37b6db1ab65905ebe6738d715944964
AndresAviles24 <130066347+AndresAviles24@users.noreply.github.com>
2023-08-30 21:16:46 -0400

    Add files via upload

---
+++ src/test/java/bo/com/andresaviles/restfulservice/RestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.com.andresaviles.restfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
